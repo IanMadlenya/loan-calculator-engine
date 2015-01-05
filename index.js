45888bfde8cce3ae8447c40ba28bead0038ae4dc
Pablo De Nadai <pablodenadai@gmail.com>
2015-01-05 21:09:10 +0000

    improve summary calculation, add extra repayments

--- index.js
+++ index.js
@@ -19,29 +19,32 @@ var _ = require("lodash");
 var CalculatorEngine = require("financial-calculator-engine"), CalculatorEngineMath = require("financial-calculator-engine/lib/math");
 
 // Loan Context class
-// Core values used in the calculation ie. `principal`, `term`...
+// Input values used in the calculation ie. `principal`, `term`...
 var LoanContext = function LoanContext(context) {
   var config = CalculatorEngine.config();
 
-  this.principal = 0;
-  this.interestRate = 0;
-  this.interestRateFrequency = config.frequency.year;
-  this.term = 0;
-  this.termFrequency = config.frequency.year;
-  this.repaymentFrequency = config.frequency.month;
+  var defaults = {
+    principal: 0,
+
+    interestRate: 0,
+    interestRateFrequency: config.frequency.year,
+    effInterestRate: 0,
+
+    term: 0,
+    termFrequency: config.frequency.year,
+    effTerm: 0,
+
+    repaymentFrequency: config.frequency.month
+  };
 
   // Extend default values with the options passed in.
-  _.merge(this, context);
-};
+  _.merge(this, defaults, context);
 
-// Calculate the interest rate per period.
-LoanContext.prototype.getEffInterestRate = function () {
-  return CalculatorEngineMath.effInterestRate(this.interestRate, this.interestRateFrequency, this.repaymentFrequency);
-};
+  // Calculate the total number of periods for a given loan.
+  this.effTerm = CalculatorEngineMath.effTerm(this.term, this.termFrequency, this.repaymentFrequency);
 
-// Calculate the total number of periods for a given loan.
-LoanContext.prototype.getEffTerm = function () {
-  return CalculatorEngineMath.effTerm(this.term, this.termFrequency, this.repaymentFrequency);
+  // Calculate the interest rate per period.
+  this.effInterestRate = CalculatorEngineMath.effInterestRate(this.interestRate, this.interestRateFrequency, this.repaymentFrequency);
 };
 
 // Loan Summary Item class
@@ -79,45 +82,26 @@ var LoanCalculatorEngine = (function () {
 
   _inherits(LoanCalculatorEngine, _CalculatorEngine);
 
-  // Returns a single object representing the current loan calculation state.
-  LoanCalculatorEngine.prototype.getContextAt = function (period) {
-    var context = _CalculatorEngine.prototype.getContextAt.call(this, period);
-    return new LoanContext(context);
-  };
-
   // Calculates a loan and its ammortization table.
   // Calculations is done on per period basis.
   LoanCalculatorEngine.prototype.calculate = function () {
-    var numberOfPeriods = this.__baseContext.getEffTerm();
+    var currentPeriod = 1, principalBalance = this.__baseContext.principal;
 
-    var summaryList = [], summaryItem = null, previousSummaryItem = null;
+    var summaryList = [];
 
-    for (var currentPeriod = 1; currentPeriod <= numberOfPeriods; currentPeriod++) {
-      if (currentPeriod > 1) {
-        previousSummaryItem = summaryList[summaryList.length - 1];
-      }
+    while (principalBalance > 0.001) {
+      var currentContext = _.clone(this.__baseContext);
 
-      // Assign the `previous principal final balance` from the summary,
-      // Use the loan principal amount instead if it's the first period (ie. no previous summary)
-      var previousPrincipalFinalBalance = previousSummaryItem ? previousSummaryItem.principalFinalBalance : this.__baseContext.principal;
+      var operators = this.getOperatorsAt(currentPeriod);
+      operators.forEach(function (operator) {
+        operator.process(currentContext);
+      });
 
-      // Select the current context
-      // It will take all active operators in account
-      var currentContext = this.getContextAt(currentPeriod);
-
-      // Calculate repayment amount (ie. pmt)
-      var interestRate = currentContext.getEffInterestRate(), numberOfPeriodsLeft = numberOfPeriods - currentPeriod + 1;
-
-      var pmt = CalculatorEngineMath.pmt(previousPrincipalFinalBalance, interestRate, numberOfPeriodsLeft);
-
-      // Create summary item
-      summaryItem = new LoanSummaryItem(currentPeriod);
-      summaryItem.principalInitialBalance = previousPrincipalFinalBalance;
-      summaryItem.pmt = pmt;
-      summaryItem.interestPaid = summaryItem.principalInitialBalance * interestRate;
-      summaryItem.principalPaid = summaryItem.pmt - summaryItem.interestPaid;
-      summaryItem.principalFinalBalance = summaryItem.principalInitialBalance - summaryItem.principalPaid;
+      var summaryItem = this.__calculateSummaryItem(currentPeriod, currentContext, principalBalance);
       summaryList.push(summaryItem);
+
+      principalBalance = summaryItem.principalFinalBalance;
+      currentPeriod++;
     }
 
     // Sum totals
@@ -132,6 +116,24 @@ var LoanCalculatorEngine = (function () {
       summaryList: summaryList,
       totals: totals
     };
+  };
+
+  LoanCalculatorEngine.prototype.__calculateSummaryItem = function (currentPeriod, currentContext, principalBalance) {
+    var effInterestRate = currentContext.effInterestRate, effExtraRepayment = currentContext.effExtraRepayment, effTermRemaining = currentContext.effTerm - currentPeriod + 1;
+
+    var pmt = CalculatorEngineMath.pmt(principalBalance, effInterestRate, effTermRemaining);
+
+    pmt += effExtraRepayment ? effExtraRepayment : 0;
+
+    // Create summary item
+    var summaryItem = new LoanSummaryItem(currentPeriod);
+    summaryItem.principalInitialBalance = principalBalance;
+    summaryItem.pmt = pmt;
+    summaryItem.interestPaid = summaryItem.principalInitialBalance * effInterestRate;
+    summaryItem.principalPaid = summaryItem.pmt - summaryItem.interestPaid;
+    summaryItem.principalFinalBalance = summaryItem.principalInitialBalance - summaryItem.principalPaid;
+
+    return summaryItem;
   };
 
   return LoanCalculatorEngine;
--- src/index.js
+++ src/index.js
@@ -6,36 +6,39 @@ var CalculatorEngine = require('financial-calculator-engine'),
 	CalculatorEngineMath = require('financial-calculator-engine/lib/math');
 
 // Loan Context class
-// Core values used in the calculation ie. `principal`, `term`...
+// Input values used in the calculation ie. `principal`, `term`...
 class LoanContext {
 	constructor(context) {
 		var config = CalculatorEngine.config();
 
-		this.principal = 0;
-		this.interestRate = 0;
-		this.interestRateFrequency = config.frequency.year;
-		this.term = 0;
-		this.termFrequency = config.frequency.year;
-		this.repaymentFrequency = config.frequency.month;
+		var defaults = {
+			principal: 0,
+
+			interestRate: 0,
+			interestRateFrequency: config.frequency.year,
+			effInterestRate: 0,
+
+			term: 0,
+			termFrequency: config.frequency.year,
+			effTerm: 0,
+
+			repaymentFrequency: config.frequency.month
+		};
 
 		// Extend default values with the options passed in.
-		_.merge(this, context);
-	}
+		_.merge(this, defaults, context);
 
-	// Calculate the interest rate per period.
-	getEffInterestRate() {
-		return CalculatorEngineMath.effInterestRate(
-			this.interestRate,
-			this.interestRateFrequency,
-			this.repaymentFrequency
-		);
-	}
-
-	// Calculate the total number of periods for a given loan.
-	getEffTerm() {
-		return CalculatorEngineMath.effTerm(
+		// Calculate the total number of periods for a given loan.
+		this.effTerm = CalculatorEngineMath.effTerm(
 			this.term,
 			this.termFrequency,
+			this.repaymentFrequency
+		);
+
+		// Calculate the interest rate per period.
+		this.effInterestRate = CalculatorEngineMath.effInterestRate(
+			this.interestRate,
+			this.interestRateFrequency,
 			this.repaymentFrequency
 		);
 	}
@@ -75,53 +78,27 @@ class LoanCalculatorEngine extends CalculatorEngine {
 		this.__baseContext = new LoanContext(context);
 	}
 
-	// Returns a single object representing the current loan calculation state.
-	getContextAt(period) {
-		var context = super.getContextAt(period);
-		return new LoanContext(context);
-	}
-
 	// Calculates a loan and its ammortization table.
 	// Calculations is done on per period basis.
 	calculate() {
-		var numberOfPeriods = this.__baseContext.getEffTerm();
+		var currentPeriod = 1,
+			principalBalance = this.__baseContext.principal;
 
-		var summaryList = [],
-			summaryItem = null,
-			previousSummaryItem = null;
+		var summaryList = [];
 
-		for (var currentPeriod = 1; currentPeriod <= numberOfPeriods; currentPeriod++) {
-			if (currentPeriod > 1) {
-				previousSummaryItem = summaryList[summaryList.length - 1];
-			}
+		while (principalBalance > 0.001) {
+			var currentContext = _.clone(this.__baseContext);
 
-			// Assign the `previous principal final balance` from the summary, 
-			// Use the loan principal amount instead if it's the first period (ie. no previous summary)
-			var previousPrincipalFinalBalance = previousSummaryItem ?
-				previousSummaryItem.principalFinalBalance : this.__baseContext.principal;
+			var operators = this.getOperatorsAt(currentPeriod);
+			operators.forEach(function(operator) {
+				operator.process(currentContext);
+			});
 
-			// Select the current context
-			// It will take all active operators in account
-			var currentContext = this.getContextAt(currentPeriod);
-
-			// Calculate repayment amount (ie. pmt)
-			var interestRate = currentContext.getEffInterestRate(),
-				numberOfPeriodsLeft = numberOfPeriods - currentPeriod + 1;
-
-			var pmt = CalculatorEngineMath.pmt(
-				previousPrincipalFinalBalance,
-				interestRate,
-				numberOfPeriodsLeft
-			);
-
-			// Create summary item
-			summaryItem = new LoanSummaryItem(currentPeriod);
-			summaryItem.principalInitialBalance = previousPrincipalFinalBalance;
-			summaryItem.pmt = pmt;
-			summaryItem.interestPaid = summaryItem.principalInitialBalance * interestRate;
-			summaryItem.principalPaid = summaryItem.pmt - summaryItem.interestPaid;
-			summaryItem.principalFinalBalance = summaryItem.principalInitialBalance - summaryItem.principalPaid;
+			var summaryItem = this.__calculateSummaryItem(currentPeriod, currentContext, principalBalance);
 			summaryList.push(summaryItem);
+
+			principalBalance = summaryItem.principalFinalBalance;
+			currentPeriod++;
 		}
 
 		// Sum totals
@@ -136,6 +113,30 @@ class LoanCalculatorEngine extends CalculatorEngine {
 			summaryList,
 			totals
 		};
+	}
+
+	__calculateSummaryItem(currentPeriod, currentContext, principalBalance) {
+		var effInterestRate = currentContext.effInterestRate,
+			effExtraRepayment = currentContext.effExtraRepayment,
+			effTermRemaining = currentContext.effTerm - currentPeriod + 1;
+
+		var pmt = CalculatorEngineMath.pmt(
+			principalBalance,
+			effInterestRate,
+			effTermRemaining
+		);
+
+		pmt += effExtraRepayment ? effExtraRepayment : 0;
+
+		// Create summary item
+		var summaryItem = new LoanSummaryItem(currentPeriod);
+		summaryItem.principalInitialBalance = principalBalance;
+		summaryItem.pmt = pmt;
+		summaryItem.interestPaid = summaryItem.principalInitialBalance * effInterestRate;
+		summaryItem.principalPaid = summaryItem.pmt - summaryItem.interestPaid;
+		summaryItem.principalFinalBalance = summaryItem.principalInitialBalance - summaryItem.principalPaid;
+
+		return summaryItem;
 	}
 }
 
